a01cf2347f37b6d52a40cd3d99be5c26408c2bc1
Luke McCulloch <tlukemcculloch@gmail.com>
2019-02-18 16:48:38 -0600

    tricky install notes

---
+++ install_tips.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 16:34:43 2019
+
+@author: luke
+"""
+
+"""
+pip install euclid
+pip install pyOpenGL
+
+pyOpenGL GLUT issue:
+    https://stackoverflow.com/questions/26700719/pyopengl-glutinit-nullfunctionerror
+
+Most critical:
+suitsparse up to date link:
+    https://pythonhosted.org/scikit-sparse/overview.html
+
+"""
