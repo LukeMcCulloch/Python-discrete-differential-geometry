e511ea7bb628727b81fdb4f45e8952ac82e7791f
Luke McCulloch <tlukemcculloch@gmail.com>
2019-02-18 17:00:23 -0600

    more build details

--- install_tips.py
+++ install_tips.py
@@ -8,13 +8,42 @@ Created on Mon Feb 18 16:34:43 2019
 
 """
 pip install euclid
+pip install plyfile
 pip install pyOpenGL
 
 pyOpenGL GLUT issue:
     https://stackoverflow.com/questions/26700719/pyopengl-glutinit-nullfunctionerror
 
+    Linux Users can just install glut using the following command:
+
+    $ sudo apt-get install freeglut3-dev
+
+
 Most critical:
 suitsparse up to date link:
     https://pythonhosted.org/scikit-sparse/overview.html
+    
+    Installing scikit-sparse requires:
+
+    Python
+    NumPy
+    SciPy
+    Cython
+    CHOLMOD
+
+On Debian/Ubuntu systems, the following command should suffice:
+
+    $ apt-get install python-scipy libsuitesparse-dev
+
+
+Then just:
+    
+Installation
+
+As usual,
+
+    $ pip install --user scikit-sparse
+
+
 
 """
